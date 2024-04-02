0709da0352fbc88707ad6350d2a2101b95723100
R. Quazi <112246222+qriz1452@users.noreply.github.com>
2024-04-02 13:20:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
